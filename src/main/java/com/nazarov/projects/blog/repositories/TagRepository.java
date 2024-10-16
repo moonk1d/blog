c1025f24c9536ba89175a36d3bf841d293490d14
Andrii Nazarov <andreu.nazarov@gmail.com>
2024-10-16 17:05:16 +0200

    - Improved query for TagRepository

--- src/main/java/com/nazarov/projects/blog/repositories/TagRepository.java
+++ src/main/java/com/nazarov/projects/blog/repositories/TagRepository.java
@@ -4,10 +4,11 @@ import com.nazarov.projects.blog.models.Tag;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
-  @Query("select t from Tag t where t.name = ?1")
-  List<Tag> findByName(String name);
+  @Query("SELECT t FROM Tag t WHERE t.name = :name")
+  List<Tag> findByName(@Param("name") String name);
 
 }
