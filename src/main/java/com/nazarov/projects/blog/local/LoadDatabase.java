61d622d69e6f2e1f13ad3fe39e192bb5e2f0532e
Andrii Nazarov <andreu.nazarov@gmail.com>
2025-04-10 16:19:42 +0200

    - Added local profile for LoadDatabase

--- src/main/java/com/nazarov/projects/blog/local/LoadDatabase.java
+++ src/main/java/com/nazarov/projects/blog/local/LoadDatabase.java
@@ -13,8 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("local")
 @Slf4j
 class LoadDatabase {
 
